1fffd1f5dd6b3b1e3a77dd3cf809f0934cbb6c58
Sebastian <jsebas85@gmail.com>
2014-04-07 16:38:26 +0200

    Javascript test. Mario reacts when keys are touched

---
+++ mario-key/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $(document).keydown(function(key) {
+        switch(parseInt(key.which,10)) {
+			case 37:
+				$('img').animate({left: "-=10px"}, 'fast');
+				break;
+			case 40:
+				$('img').animate({top: "+=10px"}, 'fast');
+				break;
+			case 38:
+				$('img').animate({top: "-=10px"}, 'fast');
+				break;
+			case 39:
+				$('img').animate({left: "+=10px"}, 'fast');
+				break;
+			default:
+				break;
+		}
+	});
+});
